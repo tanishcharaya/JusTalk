d5c952f43ab92f0a40c5e9cca3fac06f7766e6b0
Tanish Charaya <charayatanish@gmail.com>
2019-01-15 01:48:31 +0530

    Add form for messages ,show incoming messages and acknowledgement from server

---
+++ server/Utils/message.test.js
@@ -0,0 +1,11 @@
+var expect=require('expect');
+var {generateMessage}=require('./message'); 
+	describe('generateMessage',()=>{
+		it('should generate correct message object',()=>{
+			 var from='tanish';
+			 var text='Some Message';
+			 var message=generateMessage(from,text);
+			 expect(typeof message.createdAt).toBe('number');
+			 expect(message).toMatchObject({from,text});
+		});
+});
--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const http =  require('http');
 const socketIO=require('socket.io');
 const express=require('express');
 var app=express();
-
-const publicPath=path.join(__dirname,'../public');
+const {generateMessage}=require('./utils/message');
+const publicPath=path.join(__dirname,'../public');  
 const port=process.env.PORT||3000;  
 var server=http.createServer(app);
 var io=socketIO(server);
@@ -13,26 +13,15 @@ app.use(express.static(publicPath));
 
 io.on('connection',	(socket) =>{
 	console.log("User Connected"); 
-	socket.emit('newMessage',{
-			from:'Admin',
-			text: 'Welcome to the group'
-		});
-		socket.broadcast.emit('newMessage',{
-			text:'New User Joined'
-		});
-	socket.on('createMessage',(message)=>{
-		//console.log('createMessage',message);
-		// io.emit('newMessage',{
-		// 	from: message.from,
-		// 	text: message.text,
-		// 	createdAt: new Date().getTime()
-		// });	
-
-		socket.broadcast.emit('newMessage',{
-			from: message.from,
-			text: message.text,
-			//createdAt: new Date.getTime()
-		});
+	socket.emit('newMessage',generateMessage('Admin','Welcome to the Chat app'));
+	
+	socket.broadcast.emit('newMessage',generateMessage('Admin','New User Joined'));
+	
+	socket.on('createMessage',(message,callback)=>{
+		console.log('createMessage',message);
+		io.emit('newMessage',generateMessage(message.from,message.text));	
+		callback('This is Sent from the Server');
+		//socket.broadcast.emit('newMessage',generateMessage(message.from,message.text));
 	});
 
 	socket.on('disconnect',()=>{
