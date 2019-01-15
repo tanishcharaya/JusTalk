3717515e4a24e3d56f63da7c1d39b3a00ef6db95
Tanish Charaya <charayatanish@gmail.com>
2019-01-15 14:26:11 +0530

    Format Timesatmp using moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -7,16 +7,18 @@ socket.on('disconnect',function(){
 });
 
 socket.on('newMessage',function(message){
+	var formattedTime=moment(message.createdAt).format('h:mm a');
 	console.log('newMessage',message);
 	var li=$('<li></li>');
-	li.text(`${message.from}: ${message.text}`);
+	li.text(`${message.from} ${formattedTime}: ${message.text}`);
 	$('#messages').append(li);
 });
 
 socket.on('newLocationMessage',function(message){
+	var formattedTimeLocation=moment(message.createdAt).format('h:mm a');
 	var li=$('<li></li>');
 	var a =$('<a target="_blank">My Current Locatin</a>');
-	li.text(`${message.from}:`);
+	li.text(`${message.from} ${formattedTimeLocation}:`);
 	a.attr('href',message.url);
 	li.append(a);
 	$('#messages').append(li);
