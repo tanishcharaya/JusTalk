65166344ac9ac53cf5ef7a8a45423431547093bc
Tanish Charaya <charayatanish@gmail.com>
2019-01-15 14:54:04 +0530

    Added Mustache.js

--- public/js/index.js
+++ public/js/index.js
@@ -8,20 +8,25 @@ socket.on('disconnect',function(){
 
 socket.on('newMessage',function(message){
 	var formattedTime=moment(message.createdAt).format('h:mm a');
-	console.log('newMessage',message);
-	var li=$('<li></li>');
-	li.text(`${message.from} ${formattedTime}: ${message.text}`);
-	$('#messages').append(li);
+	var template=$('#message-template').html();
+	var html= Mustache.render(template,{
+		from: message.from,
+		text: message.text,
+		createdAt:formattedTime
+	});
+	$('#messages').append(html);
 });
 
 socket.on('newLocationMessage',function(message){
 	var formattedTimeLocation=moment(message.createdAt).format('h:mm a');
-	var li=$('<li></li>');
-	var a =$('<a target="_blank">My Current Locatin</a>');
-	li.text(`${message.from} ${formattedTimeLocation}:`);
-	a.attr('href',message.url);
-	li.append(a);
-	$('#messages').append(li);
+	var template=$('#location-message-template').html();
+	var html=Mustache.render(template,{
+		from: message.from,
+		url: message.url,
+		createdAt:formattedTimeLocation
+
+	});
+	$('#messages').append(html);
 });
 $('#message-form').on('submit',function(e){
 	e.preventDefault();
--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on('connection',	(socket) =>{
 	});
 
 	socket.on('createLocationMessage',(coords) =>{
-		io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
+		io.emit('newLocationMessage',generateLocationMessage('User',coords.latitude,coords.longitude));
 	});
 
 	socket.on('disconnect',()=>{
