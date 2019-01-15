4b607d1aeec0979d5a040321ea89c7f6f4e9faa1
Tanish Charaya <charayatanish@gmail.com>
2019-01-15 17:06:38 +0530

    AutoScrolling Added

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,18 @@
 var socket = io();
+
+function scrollToBottom(){
+	var messages=$('#messages');
+	var newMessage=messages.children('li:last-child');
+	var clientHeight=messages.prop('clientHeight');
+	var scrollTop=messages.prop('scrollTop');
+	var scrollHeight=messages.prop('scrollHeight');
+	var newMessageHeight=newMessage.innerHeight();
+	var lastMessageHeight=newMessage.prev().innerHeight();
+	if(clientHeight+scrollTop+lastMessageHeight+newMessageHeight>=scrollHeight)
+	{
+		messages.scrollTop(scrollHeight);
+	}
+}
 socket.on('connect',function(){
 		console.log('connected to server');
 });
@@ -15,6 +29,7 @@ socket.on('newMessage',function(message){
 		createdAt:formattedTime
 	});
 	$('#messages').append(html);
+	scrollToBottom();
 });
 
 socket.on('newLocationMessage',function(message){
